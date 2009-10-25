4639c2f0259fc90650ee30f95312463cb94b0523
Andreas Habel <mail@exceptionfault.de>
2009-10-25 11:53:35 +0100

    make the bugtracke open in the same window.

--- system/application/views/layout/navigation.php
+++ system/application/views/layout/navigation.php
@@ -6,7 +6,7 @@
         <ul>
             <li><a href="<?= site_url('news') ?>"><?=_("News")?> &laquo;</a></li>
             <li><a href="<?= site_url('development') ?>"><?=_("Development")?> &laquo;</a></li>
-            <li><a href="http://bugs.manasource.org" target="_blank"><?=_("Bugtracker")?> &laquo;</a></li>
+            <li><a href="http://bugs.manasource.org" target="_self"><?=_("Bugtracker")?> &laquo;</a></li>
             <li><a href="<?= site_url('downloads') ?>"><?=_("Downloads")?> &laquo;</a></li>
         </ul>
     </div>
