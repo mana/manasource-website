3ef0f5813598e41d59f8169a0b465540e20a6121
Andreas Habel <mail@exceptionfault.de>
2009-10-25 11:39:44 +0100

    Fixed bug in calling development view

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -14,7 +14,7 @@ class Main extends Controller {
 
     function development()
     {        
-        $this->output->showPage('development',df,234);
+        $this->output->showPage('development');
     }
 
     function downloads()
