4c7a3fb60abc45ff25b36b6c5cfd1f23afa7c9fa
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2009-10-29 09:46:38 +0100

    Added a link to the documentation wiki

--- system/application/views/layout/navigation.php
+++ system/application/views/layout/navigation.php
@@ -6,7 +6,8 @@
         <ul>
             <li><a href="<?= site_url('news') ?>"><?=_("News")?> &laquo;</a></li>
             <li><a href="<?= site_url('development') ?>"><?=_("Development")?> &laquo;</a></li>
-            <li><a href="http://bugs.manasource.org" target="_self"><?=_("Bugtracker")?> &laquo;</a></li>
+            <li><a href="http://doc.manasource.org/"><?=_("Documentation")?> &laquo;</a></li>
+            <li><a href="http://bugs.manasource.org/"><?=_("Bug Tracker")?> &laquo;</a></li>
             <li><a href="<?= site_url('downloads') ?>"><?=_("Downloads")?> &laquo;</a></li>
         </ul>
     </div>
