9265704a819e18d5f0d63a623dd3764fc550c174
Thorbjørn Lindeijer <thorbjorn.lindeijer@nokia.com>
2010-01-29 15:56:01 +0100

    Added a link to the nightly builds for Windows

--- system/application/views/download.php
+++ system/application/views/download.php
@@ -2,7 +2,8 @@
     <div class="header">Downloads</div>
     <div class="body">
 
-    <p>Nothing released yet! Meanwhile, check out the <a href="<?= site_url('development') ?>">development</a> section.</p>
+    <p>No release yet! Meanwhile, check out the <a href="<?= site_url('development') ?>">development</a> section
+       or try out a <a href="http://nightlies.manasource.org">nightly build for Windows</a>.</p>
 
    </div>
 </div>
