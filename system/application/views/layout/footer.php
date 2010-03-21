c4e1b09bc802403f2fb5808500da7ebb9aa02eba
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2010-03-21 15:40:01 +0100

    Removed validation buttons
    They're a bit ugly.

--- system/application/views/layout/footer.php
+++ system/application/views/layout/footer.php
@@ -1,10 +1,6 @@
             <div style="clear: both;">&nbsp;</div>
             </div><!-- end of content -->
         </div>
-        <div id="w3cfooter">
-            <img src="<?= base_url() ?>gfx/valid-xhtml10.png" alt="xhtml validated" />
-            <img src="<?= base_url() ?>gfx/vcss.gif" alt="css validated" />
-        </div>
     <!-- end of area -->
     </div>
 </body>
