39eff9ac9671e98997fc99042a68ca01b720fff4
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2010-03-21 15:35:34 +0100

    Adaptions for Mana 1.0.0-beta client release
    News item and downloads page update.

--- system/application/views/download.php
+++ system/application/views/download.php
@@ -2,8 +2,13 @@
     <div class="header">Downloads</div>
     <div class="body">
 
-    <p>No release yet! Meanwhile, check out the <a href="<?= site_url('development') ?>">development</a> section
-       or try out a <a href="http://nightlies.manasource.org">nightly build for Windows</a>.</p>
+    <h3>Mana client 1.0.0-beta</h3>
+    <p>The 1.0.0 release of the client is close. Meanwhile, please help us testing it by trying out this beta!</p>
+    <p>Source: <a href="http://manasource.org/files/mana-1.0.0-beta.tar.gz">mana-1.0.0-beta.tar.gz</a> (2 MB)</p>
+
+    <h3>Nightlies for Windows</h3>
+
+    <p>If you want to use the bleeding edge, but compiling is difficult on your system, you may want to try our <a href="http://nightlies.manasource.org">nightly builds for Windows</a>.</p>
 
    </div>
 </div>
